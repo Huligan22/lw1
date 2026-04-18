4404bb4b96adea892259c54a0e9dd42342cdfe41
Студент П4 <user_p4@mrmt.loc>
2026-04-18 09:13:40 +0300

    pip.php

--- plp.php
+++ plp.php
@@ -1,15 +1,16 @@
 <?php
-$sum = 8000;
+
+$sum: int = 8000;
 
 if ($sum >= 10000) {
-    $discount = 0.2;
+    $discount: int = 0.2;
 }elseif ($sum >= 5000) {
-    $discount = 0.1;
+    $discount: int = 0.1;
 } else {
-    $discount = 0;
+    $discount: int = 0;
 }
 
-$finalSum = $sum - ($sum * $discount);
+$finalSum: int = $sum - ($sum * $discount);
 
 echo "Сумма покупки: $sum руб.";
 echo "Скидка: " . ($discount * 100) . "% ";
